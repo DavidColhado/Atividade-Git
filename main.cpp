889fb90d503ec73a03a44b5e295fb9569643c99d
DavidColhado <davidcolhado@gmail.com>
2021-08-07 21:46:32 -0300

    Inicio do projeto

---
+++ main.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+float a, b, resp;
+char op;
+cout<< "Digite um numero:";
+cin>> a;
+cout<< "Digite o operador:";
+cin>> op;
+cout<< "Digite um numero:";
+cin>> b;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
